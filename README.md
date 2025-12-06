80fb8ba01d83b1a678bb38b0d547a5f3b4f1e685
QuantumRizo <davidrizo.phys@gmail.com>
2025-12-05 20:37:05 -0600

    feat: Implementacion completa (primera parte)  y tests pasando

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-[![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/x4cR9XE4)
-[![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-2e0aaae1b6195c2367325f4f02e2d04e9abb55f0b24a779b69b11b9e10269abc.svg)](https://classroom.github.com/online_ide?assignment_repo_id=21908433&assignment_repo_type=AssignmentRepo)
+# Estructuras Auxiliares del Compilador
+
+Proyecto final (Primera Parte) para la asignatura de Compiladores 2026-1, UNAM.
+Implementación de la infraestructura base para el análisis semántico.
+
+## Integrantes
+* Félix David Rizo Serrano
+* Pongan sus nombres, jaja
+
+## Ejecución
+
+El proyecto usa un Makefile para gestionar la descarga de GoogleTest y la compilación.
+
+### Compilar y probar
+```bash
+make
+```
--- test/test_types.cpp
+++ test/test_types.cpp
@@ -4,25 +4,26 @@
 TEST(TypeTableTest, InsertAndRetrieveBasicType) {
     TypeTable tt;
 
-    int id = tt.insertType("int", 4);
-    auto t = tt.getType(id);
+    // cambio que hice insertType -> addBasicType
+    int id = tt.addBasicType("int", 4);
+    auto t = tt.get(id);
 
     EXPECT_EQ(t.name, "int");
     EXPECT_EQ(t.size, 4);
-    EXPECT_EQ(t.numElements, 1);
+    EXPECT_EQ(t.elements, 1);
     EXPECT_EQ(t.baseTypeId, -1);
 }
 
 TEST(TypeTableTest, InsertArrayType) {
     TypeTable tt;
 
-    int baseId = tt.insertType("float", 8);
-    int arrId = tt.insertArrayType("float[]", baseId, 5);
+    int baseId = tt.addBasicType("float", 8);
+    int arrId = tt.addArrayType(baseId, 5);
 
-    auto arrType = tt.getType(arrId);
-    auto baseType = tt.getType(baseId);
+    auto arrType = tt.get(arrId);
+    auto baseType = tt.get(baseId);
 
     EXPECT_EQ(arrType.size, baseType.size * 5);
-    EXPECT_EQ(arrType.numElements, 5);
+    EXPECT_EQ(arrType.elements, 5);
     EXPECT_EQ(arrType.baseTypeId, baseId);
 }
